fc3d50d4e6ac64a7fbc343ac12706058a003b26f
Emerald Skinner <emeraldskinner25@gmail.com>
2021-06-14 16:33:31 -0600

    created an API project with models and controllers, they are incomplete

---
+++ LunchAPI/LunchAPI/Models/ApiContext.cs
@@ -0,0 +1,35 @@
+﻿using LunchAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchAPI.Models
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<School> Schools { get; set; }
+
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) //do not use this to directly create an instance of this class
+        {
+        }
+
+        /**<summary>Configures connection to the database, automatically called for each instance</summary>
+         */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer(connectionString)
+                //base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+
+    }
+}
---
+++ LunchAPI/LunchAPI/Models/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchAPI.Models
+{
+    public class Student
+    {
+		private string _StudentID;
+		private string _Name;
+		private string _SchoolID;
+		private double _Balance;
+		private string _MedicalInfo;
+			
+		//private setter is for EF Core
+		[Key, Required, DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string StudentID { get { return _StudentID; } private set { _StudentID = value; } }
+
+		[Required]
+		public string Name { get { return _Name; } private set { _Name = value; } }
+
+		[Required]
+		public string SchoolID { get { return _SchoolID; } set { _SchoolID = value; } }
+
+		[Column(TypeName = "decimal(18, 2)"), Required] //computed column - sum of student's transactions
+		public double Balance { get { return _Balance; } set { _Balance = value; } }
+
+		[Required(AllowEmptyStrings = true)]
+		public string MedicalInfo { get { return _MedicalInfo; } set { _MedicalInfo = value; } }
+
+
+		private Student(string StudentID, string Name, string SchoolID, double Balance, string MedicalInfo)
+		{
+			_StudentID = StudentID;
+			_Name = Name;
+			_SchoolID = SchoolID;
+			_Balance = Balance;
+			_MedicalInfo = MedicalInfo;
+		}
+	}
+}
