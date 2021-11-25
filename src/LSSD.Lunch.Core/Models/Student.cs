c78fabd5a0c7ae7152f2a411ed30517269d2bf4b
Mark Strendin <mark.strendin@lskysd.ca>
2021-11-25 15:29:16 -0600

    Add concept of a homeroom

--- src/LSSD.Lunch.Core/Models/Student.cs
+++ src/LSSD.Lunch.Core/Models/Student.cs
@@ -11,11 +11,11 @@ namespace LSSD.Lunch
     public class Student : IGUIDable
     {    
         public Guid Id { get; set; }
-
 		public string StudentId { get; set; }
 		public string Name { get;set ;}
 		public Guid SchoolId { get; set; }
 		public string MedicalInfo { get; set; }
 		public bool IsActive { get; set; }
+		public string HomeRoom { get; set; }
 	}
 }
