1e4b485ab2dbbd02608e20453f39309019fe9938
Mark Strendin <mark.strendin@lskysd.ca>
2022-08-29 11:34:29 -0600

    Updates for start of school 2022 (#10)
    add a year rollover system
    Remove azure key vault, favouring dotnet user-secrets instead
    Separate active and all student lists in the management site

--- src/LSSD.Lunch.WebScanner/Program.cs
+++ src/LSSD.Lunch.WebScanner/Program.cs
@@ -3,41 +3,19 @@ using Microsoft.AspNetCore.Components.Web;
 using LSSD.Lunch.WebScanner.Services;
 using LSSD.MongoDB;
 using LSSD.Lunch;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 var builder = WebApplication.CreateBuilder(args);
-
-IConfiguration configuration = new ConfigurationBuilder()
-                   .AddEnvironmentVariables()
-                   .Build();
-
-string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT") ?? string.Empty;
-
-if (!string.IsNullOrEmpty(GetKeyVaultEndpoint())) 
-{
-    Console.WriteLine("Retrieving configuration from Azure Key Vault (" + GetKeyVaultEndpoint() + ")");
-    var azureServiceTokenProvider = new AzureServiceTokenProvider();
-    var keyVaultClient = new KeyVaultClient(
-    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-
-    configuration = new ConfigurationBuilder()
-                   .AddEnvironmentVariables()
-                   .AddAzureKeyVault(GetKeyVaultEndpoint(), keyVaultClient, new DefaultKeyVaultSecretManager())
-                   .Build();
-}
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddLocalization();
-builder.Configuration.AddConfiguration(configuration);
+
 builder.Services.AddScoped<SchoolService>();
 builder.Services.AddScoped<FoodItemService>();
 builder.Services.AddScoped<StudentService>();
 builder.Services.AddScoped<TransactionService>();
-builder.Services.AddSingleton<MongoDbConnection>(x => new MongoDbConnection(configuration.GetConnectionString("InternalDatabase")));
+builder.Services.AddSingleton<MongoDbConnection>(x => new MongoDbConnection(builder.Configuration.GetConnectionString("InternalDatabase")));
 builder.Services.AddSingleton<IRepository<School>, MongoRepository<School>>();
 builder.Services.AddSingleton<IRepository<Student>, MongoRepository<Student>>();
 builder.Services.AddSingleton<IRepository<Transaction>, MongoRepository<Transaction>>();
